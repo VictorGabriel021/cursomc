43840b26c1e61ddef7b2acb22192915edd3b273d
Victor Gabriel <victorgabriel.cipa@gmail.com>
2018-10-24 14:49:17 -0300

    Projeto criado

---
+++ src/main/java/com/victor/cursomc/CursosmcApplication.java
@@ -0,0 +1,12 @@
+package com.victor.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosmcApplication.class, args);
+	}
+}
